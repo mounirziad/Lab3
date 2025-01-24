d138bc123ca9466c28f682a7f2c01a4c01d0da32
da413 <danielwolfcol@gmail.com>
2025-01-23 21:29:04 -0500

    commented on challenge 2

--- Assets/Scripts/Challenge2.cs
+++ Assets/Scripts/Challenge2.cs
@@ -24,22 +24,28 @@ public class Challenge2 : MonoBehaviour
 
     private float profit;
 
+    //defining variables 
+
 
     // Start is called before the first frame update
     void Start()
     {
 
-        float discountedpriceofbook = (priceofbook * discount);
+        float discountedpriceofbook = (priceofbook * discount); //calculates the price of one book bought by the bookstore
 
-        float wholesalecost = (discountedpriceofbook + 0.75f) * (amountofbooksbought - 1) + (discountedpriceofbook + 3);
+        float wholesalecost = (discountedpriceofbook + 0.75f) * (amountofbooksbought - 1) + (discountedpriceofbook + 3); 
+        //calculates the wholesale cost or the cost to buy books. (discountedpriceofbook+3) is the price of the first book bought and (discountedpriceofbook+0.75f)*(amountofbooksbought-1) is the price of all subsequent books bought
 
         float revenue = priceofbook * amountofbookssold;
+        //revenue is calculated by the retail cost of a book multiplied by the number of books sold 
 
-        float profit = revenue - wholesalecost;
+        float profit = revenue - wholesalecost; 
+        //calculated by substracting the cost of goods from the revenue made 
         Debug.Log("The wholesalecost is ");
         Debug.Log(wholesalecost);
         Debug.Log("The Profit is ");
         Debug.Log(profit);
+        //print to console
     }
 
     // Update is called once per frame
